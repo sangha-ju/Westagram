4bd3b399821c113bb6ffe1e0008a2d37398fdfd7
sangha-ju <48509329+sangha-ju@users.noreply.github.com>
2020-04-09 14:30:58 +0900

    input eventListener commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-(function() {
-    
-    const changeLogElem = document.querySelector(".logput");
+const changeLog = document.querySelector('.logPut');
+const changeBtn = document.querySelector('.logBtn');
 
-    changeLogElem.addEventListener("keydown", function(e) {
-        
-    });
-}
-
-)();
+changeLog.addEventListener('keydown', function(e) {
+    if(changeLog.value.length > 0) {
+        changeBtn.style.backgroundColor = "blue";    
+    } else {
+        changeBtn.style.backgroundColor = "rgba(0, 149, 246, 0.3)";
+    }
+});
