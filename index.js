9f77ab83696dda97a1b780e9fe9945f1bc1c2d02
sangha-ju <48509329+sangha-ju@users.noreply.github.com>
2020-04-10 12:55:34 +0900

    pwdPut BtnEventListener commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const changeLog = document.querySelector('.logPut');
+const changePwd = document.querySelector('.pwdPut');
 const changeBtn = document.querySelector('.logBtn');
 
-changeLog.addEventListener('keydown', function(e) {
+changePwd.addEventListener('keyup', function(e) {
     if(changeLog.value.length > 0) {
-        changeBtn.style.backgroundColor = "blue";    
-    } else {
+        if(changePwd.value.length > 0){
+            changeBtn.style.backgroundColor = "blue"; 
+        } else {
         changeBtn.style.backgroundColor = "rgba(0, 149, 246, 0.3)";
-    }
+        }
+    } 
 });
